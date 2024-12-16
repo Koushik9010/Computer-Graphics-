ac9ddd94329f7de77bfba701a4f2cc7636108fbe
Koushik9010 <123968866+Koushik9010@users.noreply.github.com>
2024-12-16 14:12:43 +0600

    Add files via upload

---
+++ spring2.cpp
@@ -0,0 +1,28 @@
+#include<graphics.h>
+
+void glassFace(){
+    //face
+    circle(200,300,60);
+    //eyes
+    line(180, 280, 175, 280);
+    line(230, 280, 225, 280);
+
+    //glass
+    circle(165,270,30);
+    circle(240,270,30);
+    line(210, 260, 190, 260);
+
+    //mouth
+    arc(200, 310, 180, 360, 15);
+}
+
+int main() {
+    initwindow(600, 600);
+    setbkcolor(WHITE);
+    cleardevice();
+    setcolor(BLACK);
+    glassFace();
+    getch();
+    closegraph();
+    return 0;
+}
