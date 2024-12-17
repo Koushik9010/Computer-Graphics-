0a603b27a1a17a2b1a7022568383288931381e8c
Koushik9010 <123968866+Koushik9010@users.noreply.github.com>
2024-12-17 10:31:13 +0600

    Update autumn7.cpp

--- autumn7.cpp
+++ autumn7.cpp
@@ -13,20 +13,27 @@ int main(){
     //right
     line(280, 250, 280, 300);
 
+    //middle lines
     line(150, 250, 190, 250);
     line(240, 250, 280, 250);
 
+    //middle of base and middle line
     line(200, 270, 230, 270);
 
+    //curved line
     line(190,250,200,270);
     line(240,250,230,270);
 
+    //upper left
     line(160, 150, 150, 250);
+    //upper right
     line(260, 150, 280, 250);
 
+    //top
     line(160, 150, 200, 150);
     line(230, 150, 260, 150);
 
+    //middle arrow
     line(215, 130, 215, 210);
     line(200,190,215,210);
     line(225,190,215,210);
